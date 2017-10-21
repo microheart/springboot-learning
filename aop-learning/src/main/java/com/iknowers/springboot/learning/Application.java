794bef822cdf8300ce476a64e55222ef7b1e4f3c
长烽 <xushun.xs@alibaba-inc.com>
2017-10-21 21:34:35 +0800

    删除无用代码。

--- aop-learning/src/main/java/com/iknowers/springboot/learning/Application.java
+++ aop-learning/src/main/java/com/iknowers/springboot/learning/Application.java
@@ -1,16 +1,10 @@
 package com.iknowers.springboot.learning;
 
-import javax.annotation.Resource;
-
-import com.iknowers.springboot.learning.aop.MockService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
-	@Resource
-	private MockService service;
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Application.class, args);
